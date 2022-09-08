fd7c659606b94feb734b21912dd491d01cecb0e5
Nagarjun <111555377+nagarjun7397@users.noreply.github.com>
2022-09-08 14:15:51 +0530

    added dropdown and finishing touches

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // STICKY HEADER //
-window.onscroll = function() {myFunction()};
+window.onscroll = myFunction()
 
 var header = document.getElementById("myHeader");
 var sticky = header.offsetTop;
@@ -11,3 +11,21 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 }
+
+//DROPDOWN//
+
+function myDropfunction() {
+    document.querySelector(".dropdown-menu").classList.add("visible");
+}
+
+function myDropupfunction() {
+    document.querySelector(".dropdown-menu").classList.remove("visible");
+}
+
+//var dropdown = document.querySelector(".dropdown-menu");
+//var button = document.querySelector(".dropdown-button");
+//
+//
+//button.addEventListener("click", () => {
+//  dropdown.classList.add("visible");
+//});
